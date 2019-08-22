80d75bd1885a54c459190855d21de0b9e1b65bdf
APCS2nd <APCS2nd@MHS-LT9-13>
2019-08-22 11:28:33 -0700

    Commit

---
+++ Hello World/src/Hello_World.java
@@ -0,0 +1,13 @@
+
+public class Hello_World {
+
+	public static void main(String[] args) {System.out.println("Hello World!\n");
+		// TODO Auto-generated method stub
+	System.out.println("Good Bye World!");
+	}
+
+}
+
+// John
+// 8/16/19
+// This is just a simple Hello World and Good Bye World!
