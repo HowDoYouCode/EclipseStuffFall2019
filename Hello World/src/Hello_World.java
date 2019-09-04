532d8eb94860faf579984b5a6d5b5faf3e560abc
APCS2nd <APCS2nd@MHS-LT9-13>
2019-09-04 12:22:40 -0700

    Hello World

--- Hello World/src/Hello_World.java
+++ Hello World/src/Hello_World.java
@@ -1,13 +1,8 @@
 
 public class Hello_World {
 
-	public static void main(String[] args) {System.out.println("Hello World!\n");
+	public static void main(String[] args) {System.out.println("Hello World!");
 		// TODO Auto-generated method stub
-	System.out.println("Good Bye World!");
 	}
 
 }
-
-// John
-// 8/16/19
-// This is just a simple Hello World and Good Bye World!
