f46420d3a582f2c59b5666cb4ea6ed60aff66fa9
John <John@DESKTOP-RQD67BS>
2019-10-10 21:25:42 -0700

    Calculate Library Project Finished?

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -6,28 +6,28 @@
 public class DoMath {
 
 	public static void main(String[] args) {
-		System.out.println("Square: "+Calculate.square(-3));
+		System.out.println("Square: "+Calculate.square(5));
 		System.out.println("Cube: "+Calculate.cube(-2));
-		System.out.println("Average: "+Calculate.average(3.0, 9.0));
-		System.out.println("AverageOverloaded: "+Calculate.average(15.0, 17.0, 18.0));
-		System.out.println("ToDegrees: "+Calculate.toDegrees(2));
-		System.out.println("ToRadians: "+Calculate.toRadians(1));
-		System.out.println("Discriminant: "+Calculate.discriminant(2, 4, -2));//32
-		System.out.println("ToImproperFrac: "+Calculate.toImproperFrac(2, 1, 4));//9/4
-		System.out.println("ToMixedNum: "+Calculate.toMixedNum(7, 2));
-		System.out.println("Foil: "+Calculate.foil(2, 3, 6, -7, "n"));
-		System.out.println("IsDivisibleBy: "+Calculate.isDivisibleBy(1, 2));
+		System.out.println("Average: "+Calculate.average(5.0, 9.0));
+		System.out.println("AverageOverloaded: "+Calculate.average(15.0, 18.0, 18.0));
+		System.out.println("ToDegrees: "+Calculate.toDegrees(1));
+		System.out.println("ToRadians: "+Calculate.toRadians(2));
+		System.out.println("Discriminant: "+Calculate.discriminant(7, 5, -2));//32
+		System.out.println("ToImproperFrac: "+Calculate.toImproperFrac(8, 1, 4));//9/4
+		System.out.println("ToMixedNum: "+Calculate.toMixedNum(7, 72));
+		System.out.println("Foil: "+Calculate.foil(2, 2, 2, -2, "n"));
+		System.out.println("IsDivisibleBy: "+Calculate.isDivisibleBy(1, 1));
 		System.out.println("AbsValue: "+Calculate.absValue(-22));
 		System.out.println("Max: "+Calculate.max(22, 11));
-		System.out.println("MaxOverload: "+Calculate.max(23, 12, -63));
+		System.out.println("MaxOverload: "+Calculate.max(23, 12, -62));
 		System.out.println("Min: "+Calculate.min(22, 11));
 		System.out.println("Round: "+Calculate.round2(5.051));
-		System.out.println("Exponent: "+Calculate.exponent(5, 2));
-		System.out.println("Factorial: "+Calculate.factorial(7));
-		System.out.println("IsPrime: "+Calculate.isPrime(3));
-		System.out.println("GreatestCommonFactor: "+Calculate.gcf(-34, 12));
-		System.out.println("SquareRoot: "+Calculate.sqrt(50));
-		System.out.println("QuadraticFormula: "+Calculate.quadForm(2, -7, 1));
+		System.out.println("Exponent: "+Calculate.exponent(1, 0));
+		System.out.println("Factorial: "+Calculate.factorial(5));
+		System.out.println("IsPrime: "+Calculate.isPrime(1));
+		System.out.println("GreatestCommonFactor: "+Calculate.gcf(-52, 172));
+		System.out.println("SquareRoot: "+Calculate.sqrt(0));
+		System.out.println("QuadraticFormula: "+Calculate.quadForm(-2, 2, -2));
 		// TODO Auto-generated method stub
 
 	}
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -4,49 +4,45 @@
  * 4th period
  * */
 public class Calculate {
-	//returns the square of the value passed.
-		public static int square(int number) {
-			return number * number;
-		}
 		public static double square(double number) {
 			return number * number;
 		}
-		//returns the cube of an int
+		//returns the cube of an int.
 		public static int cube(int number) {
 			return number * number * number;
 		}
-		//returns the average of two doubles
+		//returns the average of two doubles.
 		public static double average(double number1, double number2) {
 			return (number1 + number2)/2; 
 		}
-		//overloaded method. returns average of three doubles
+		//overloaded method. returns average of three doubles.
 		public static double average(double number1, double number2, double number3) {
 			return (number1 + number2 + number3)/3;
 		}
-		//returns degrees(double) of the inputed radians(double)
+		//returns double degrees of the inputed double radians.
 		public static double toDegrees(double radians) {
 			return radians * 180 / 3.14159;
 		}
-		//returns radians(double) of the inputed degrees(double)
+		//returns double radians of the inputed double degrees.
 		public static double toRadians(double degrees) {
 			return degrees * 3.14159 / 180;
 		}
-		//returns discriminant from 3 doubles a,b,c
+		//returns discriminant from 3 doubles a,b,c.
 		public static double discriminant(double a, double b, double c) {
 			return b * b - 4 * a * c;
 		}
-		//returns improper fraction(String) when input mixed number(3 ints)
+		//returns improper fraction(String) when input mixed number 3 ints.
 		public static String toImproperFrac(int wholeNum, int numer, int denom) {
 			int fracNumer = denom * wholeNum + numer;
 			return fracNumer + "/" + denom;
 		}
-		//returns mixed number(String) when input numerator and denominator 
+		//returns mixed number(String) when input numerator and denominator. 
 		public static String toMixedNum(int numer, int base) {
 			int wholeNum = numer / base;
 			int newNumer = numer % base;
 			return wholeNum +"_" + newNumer + "/" + base;
 		}
-		//converts (ax+b)(cx+d) to ax^2 + bx + c 
+		//converts (ax+b)(cx+d) to ax^2 + bx + c.
 		public static String foil(int a, int b, int c, int d, String x) {
 			int f = a * c;
 			int o = a * d;
@@ -55,7 +51,7 @@ public class Calculate {
 			int oi = o + i;
 			return f + x + "^2" + " + " + oi + x + " + " + l;
 		}
-		//returns boolean base on if int a is divisible by int b
+		//returns boolean base on if int a is divisible by int b.
 		public static boolean isDivisibleBy(int number1, int number2) {
 			if(number2 == 0)throw new IllegalArgumentException("Imagine that you have zero cookies and you split them evenly among zero friends. How many cookies does each person get? See? It doesn't make sense. And Cookie Monster is sad that there are no cookies, and you are sad that you have no friends. (▀̿Ĺ̯▀̿ ̿)");
 			if(number1 % number2 == 0) {
@@ -64,7 +60,7 @@ public class Calculate {
 				return false;
 			}
 		}
-		//returns absolute value(double) of inputed double 
+		//returns absolute value of a double of inputed double.
 		public static double absValue(double number) {
 			if(number < 0) {
 				return -number;
@@ -72,7 +68,7 @@ public class Calculate {
 				return number;
 			}
 		}
-		//returns the larger value double of the two inputed doubles
+		//returns the larger value double of the two inputed doubles.
 		public static double max(double number1, double number2) {
 			if(number1 > number2) {
 				return number1;
@@ -80,7 +76,7 @@ public class Calculate {
 				return number2;
 			}
 		}
-		//returns the larger value double of the three inputed doubles
+		//returns the larger value double of the three inputed doubles.
 		public static double max(double number1, double number2, double number3) {
 			if(number1 > number2) {
 				if(number1 > number3) {
@@ -96,14 +92,14 @@ public class Calculate {
 				}
 			}
 		}
-		//returns the smaller value int of the two inputed int
+		//returns the smaller value int of the two inputed int numbers.
 		public static int min(int number1, int number2) {
 			if(number1 < number2) {
 				return number1;
 			}else
 				return number2;
 		}
-		//returns a double rounded to 2 decimal places from inputed double
+		//returns a double rounded to 2 decimal places from inputed double.
 		public static double round2(double dec) {
 			if(dec > 0) {
 				dec += 0.005;
@@ -116,9 +112,10 @@ public class Calculate {
 			double b = (int)dec;
 			b /= 100;
 			return b;
-		}//returns value(double) for base^exp
+		}//returns value of a double for base^exp
 		public static double exponent(double base, int exp) {
 			if(exp < 0)throw new IllegalArgumentException("Error cannot compute negative exponent (╯ ͠° ͟ʖ ͡°)╯┻━┻");
+			if(exp == 0 & base == 0)throw new IllegalArgumentException("undefined");
 			double baseHolder = base;
 			if(exp == 0) {
 				return 1;
@@ -127,7 +124,7 @@ public class Calculate {
 				baseHolder *= base;	
 			}
 			return baseHolder;
-		}//returns factorial of inputed num
+		}//returns factorial of inputed int number.
 		public static int factorial(int number) {
 			if(number < 0)throw new IllegalArgumentException("Cannot input negative number ¯\\_(ツ)_/¯");
 			if(number == 0){
@@ -137,7 +134,7 @@ public class Calculate {
 				number *= count;
 			}
 			return number;
-		}//returns boolean(true or false) according if it is a prime number
+		}//returns boolean according if it is a prime number.
 		public static boolean isPrime(int a) {
 			int b = 2;
 			while(b < a) {
@@ -150,7 +147,7 @@ public class Calculate {
 				}
 			}
 			return true;
-		}//returns the greatest common factor of num1 and num2
+		}//returns the greatest common factor of number1 and number2.
 		public static int gcf(int number1, int number2) {
 			while (number2 != 0) {
 				int replace = number1;
@@ -161,24 +158,24 @@ public class Calculate {
 			absValue(a);
 			number1= (int)a;
 			return number1;
-		}//returns square root rounded to 2 decimal places with Newton's fancy method
+		}//returns square root rounded to 2 decimal places with Newton's fancy method.
 		public static double sqrt(double number) {
 			if(number < 0)throw new IllegalArgumentException("You can't SquareRoot a negative number ಠ_ಠ");
 			double value = 1; 
-			while(!(absValue(number - square(value)) < 0.001)) {
+			while(!(absValue(number - square(value)) < 0.0000001)) {
 				value = 0.5 * (number / value + value);
 			}
 			return round2(value);
-		}//returns a String with the value of the output of the quadratic formula when inputed a,b,c unless no real roots
+		}//returns a String with the value of the output of the quadratic formula when inputed a,b,c unless it has no real roots.
 		public static String quadForm(int a, int b, int c) {
 			if(discriminant(a, b, c) < 0) {
 				return "no real roots";
 			}
-			double plusAns = (-b + sqrt(discriminant(a,b,c))) / (2 * a);
-			double minusAns = (-b - sqrt(discriminant(a,b,c))) / (2 * a);
-			if(plusAns == minusAns) {
-				return round2(plusAns) + "";
+			double max = (-b + sqrt(discriminant(a,b,c))) / (2 * a);
+			double min = (-b - sqrt(discriminant(a,b,c))) / (2 * a);
+			if(max == min) {
+				return round2(max) + "";
 			}
-			return round2(plusAns) + " and " + round2(minusAns);
+			return round2(min) + " and " + round2(max);
 		}
 }
