cfa51b7362c563fa0baa1a8bfd13abb15a6bece2
JOHN <APCS2nd@MHS>
2019-10-30 13:03:28 -0700

    Commit

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,12 +1,15 @@
 package fracCalc;
+import java.util.Scanner;
 
 public class FracCalc {
 
     public static void main(String[] args) 
     {
         // TODO: Read the input from the user and call produceAnswer with an equation
-
-    }
+    	Scanner Userin = new Scanner(System.in);
+    	String input = Userin.nextLine();
+    	producseAnswer(input);
+    	}
     
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
     // This function takes a String 'input' and produces the result
