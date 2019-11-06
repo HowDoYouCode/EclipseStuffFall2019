4b9b690337b1cf9b688bd87e05677c617c7f722a
JOHN <APCS2nd@MHS>
2019-11-06 12:51:27 -0800

    needs work

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,10 +1,10 @@
 package fracCalc;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class FracCalc {
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         // TODO: Read the input from the user and call produceAnswer with an equation
     	Scanner Userin = new Scanner(System.in);
     	String input = Userin.nextLine();
@@ -23,12 +23,25 @@ public class FracCalc {
     //        
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    { 
+    public static String produceAnswer(String input) { 
         // TODO: Implement this function to produce the solution to the input
+    	String denominator = "1";
+    	String numerator = "0";
+    	String whole = "0";
         String[] operand = input.split(" ");
-        String Answer = operand[2];
-    	return Answer;  
+        String[] Answerss = operand[2].split("/");
+        denominator = Answerss[1];
+        for (int i = 0; i<Answerss[0].length();i++) {
+        	if (Answerss[0].charAt(i)=='_') {
+        		String[] Answers = Answerss[0].split("_");
+        		whole = Answers[0];
+        		numerator = Answers[1];
+        	} else {
+        		whole = "0";
+        		numerator = Answerss[0];
+        	}
+        }
+    	return (whole+numerator+denominator);  
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
